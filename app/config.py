318640a257c3a7022d11136920e926c894fb41b8
rfsalman <salman@gradienttech.io>
2023-04-25 16:48:12 +0700

    build: project structure

---
+++ app/config.py
@@ -0,0 +1,15 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    OPENAI_API_SECRET_KEY: str
+    DATABASE_URL: str
+    MONGO_INITDB_DATABASE: str
+
+    CLIENT_ORIGIN: str
+
+    class Config:
+        env_file = './.env'
+
+
+settings = Settings()
---
+++ app/main.py
@@ -0,0 +1,45 @@
+import openai
+import os
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+openai.api_key = os.environ["OPENAI_API_SECRET_KEY"]
+
+models = openai.Model.list()
+
+
+class Message(BaseModel):
+    content: str
+
+
+@app.post("/chats/{chat_id}/message")
+def post_message(chat_id: int, message: Message):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message.content}
+        ]
+    )
+
+    return {"response": completion['choices'][0]['message']['content']}
